dad9793c286f4323446ada6dd01a5e1089153e3a
Lovefocux <olabodejoshuaf@gmail.com>
2022-04-02 17:39:11 -0700

    a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+int _putchar(char c)
+{
+int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+int main(int argc, char *argv[])
+}
