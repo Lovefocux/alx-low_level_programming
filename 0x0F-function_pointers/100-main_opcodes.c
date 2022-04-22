3be3aaed671f563079c4d7a06cf750f9f546619d
Lovefocux <olabodejoshuaf@gmail.com>
2022-04-21 17:23:38 -0700

    a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,10 +1,39 @@
-#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* op_sub -
-* Return:
+* main - entry point
+* @argc: argument count
+* @argv: argument vector
+* Return: always 0
 */
 
-int op_sub(int a, int b)
+int main(int argc, char *argv[])
 {
+	int i, n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < n - 1)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	return (0);
 }
